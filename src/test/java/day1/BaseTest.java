b27a954f31b43eb13fec5594236dbb02c63ab9d5
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-07-15 18:50:34 +0530

    script updated

--- src/test/java/day1/BaseTest.java
+++ src/test/java/day1/BaseTest.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	
 	WebDriver driver;
 	
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rudreshwar.Jha\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rudreshwar.Jha\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
